241dfca6355fa1f63358aa89dfaab8f99d3a7ce3
eMJay0921 <michael@newlogic.com>
2023-05-04 07:23:09 +0000

    add scanned qr field and record id field for recordid fetching

--- spp_opencrvs_demo/models/cr_add_child.py
+++ spp_opencrvs_demo/models/cr_add_child.py
@@ -3,7 +3,7 @@ import logging
 
 import requests
 
-from odoo import fields, models
+from odoo import _, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -27,33 +27,75 @@ class ChangeRequestTypeCustomAddChildMember(models.Model):
     )
 
     brn = fields.Char("BRN")
+    crvs_qr = fields.Text("Scanned QR")
+    crvs_record_id = fields.Char("Record ID")
 
     def fetch_data_from_opencrvs(self):
         for rec in self:
             if rec.crvs_config_id:
-                data = {
-                    "operationName": "searchEvents",
-                    "query": "query searchEvents($advancedSearchParameters: AdvancedSearchParametersInput!, $sort: String, $count: Int, $skip: Int) {\nsearchEvents(\n  advancedSearchParameters: $advancedSearchParameters\n  sort: $sort\n  count: $count\n  skip: $skip\n) {\n  totalItems\n  results {\n    id\n    type\n    registration {\n      status\n      contactNumber\n      trackingId\n      registrationNumber\n      registeredLocationId\n      duplicates\n      assignment {\n        userId\n        firstName\n        lastName\n        officeName\n        __typename\n      }\n      createdAt\n      modifiedAt\n      __typename\n    }\n    operationHistories {\n      operationType\n      operatedOn\n      operatorRole\n      operatorName {\n        firstNames\n        familyName\n        use\n        __typename\n      }\n      operatorOfficeName\n      operatorOfficeAlias\n      notificationFacilityName\n      notificationFacilityAlias\n      rejectReason\n      rejectComment\n      __typename\n    }\n    ... on BirthEventSearchSet {\n      dateOfBirth\n      childName {\n        firstNames\n        familyName\n        use\n        __typename\n      }\n      __typename\n    }\n    ... on DeathEventSearchSet {\n      dateOfDeath\n      deceasedName {\n        firstNames\n        familyName\n        use\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}}",  # noqa: B950
-                    "variables": {
-                        "advancedSearchParameters": {"registrationNumber": rec.brn}
-                    },
-                }
+                data = {}
                 rec.crvs_config_id.on_authenticate()
                 headers = {
                     "Content-Type": "application/json",
                     "Accept": "application/json",
                     "Authorization": "bearer {}".format(rec.crvs_config_id.auth_token),
                 }
-                response = requests.post(
-                    rec.crvs_config_id.domain,
-                    data=json.dumps(data),
-                    headers=headers,
-                )
-                if response.status_code == 200:
-                    response_json = response.json()
-                    response_json = response_json["data"]["searchEvents"]["results"][0]
+                if not rec.crvs_qr and not rec.crvs_record_id:
+                    data = {
+                        "operationName": "searchEvents",
+                        "query": "query searchEvents($advancedSearchParameters: AdvancedSearchParametersInput!, $sort: String, $count: Int, $skip: Int) {\nsearchEvents(\n  advancedSearchParameters: $advancedSearchParameters\n  sort: $sort\n  count: $count\n  skip: $skip\n) {\n  totalItems\n  results {\n    id\n    type\n    registration {\n      status\n      contactNumber\n      trackingId\n      registrationNumber\n      registeredLocationId\n      duplicates\n      assignment {\n        userId\n        firstName\n        lastName\n        officeName\n        __typename\n      }\n      createdAt\n      modifiedAt\n      __typename\n    }\n    operationHistories {\n      operationType\n      operatedOn\n      operatorRole\n      operatorName {\n        firstNames\n        familyName\n        use\n        __typename\n      }\n      operatorOfficeName\n      operatorOfficeAlias\n      notificationFacilityName\n      notificationFacilityAlias\n      rejectReason\n      rejectComment\n      __typename\n    }\n    ... on BirthEventSearchSet {\n      dateOfBirth\n      childName {\n        firstNames\n        familyName\n        use\n        __typename\n      }\n      __typename\n    }\n    ... on DeathEventSearchSet {\n      dateOfDeath\n      deceasedName {\n        firstNames\n        familyName\n        use\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}}",  # noqa: B950
+                        "variables": {
+                            "advancedSearchParameters": {"registrationNumber": rec.brn}
+                        },
+                    }
 
-                    _logger.info(response_json)
-                    rec.birthdate = response_json["dateOfBirth"]
-                    rec.given_name = response_json["childName"][0]["firstNames"]
-                    rec.family_name = response_json["childName"][0]["familyName"]
+                else:
+                    if rec.crvs_qr and not rec.crvs_record_id:
+                        crvs_qr = rec.crvs_qr.rsplit("/", 1)[-1]
+                        rec.crvs_record_id = crvs_qr
+
+                    recordid = rec.crvs_record_id
+                    data = {
+                        "operationName": "searchEvents",
+                        "query": "query searchEvents($advancedSearchParameters: AdvancedSearchParametersInput!, $sort: String, $count: Int, $skip: Int) {\nsearchEvents(\n  advancedSearchParameters: $advancedSearchParameters\n  sort: $sort\n  count: $count\n  skip: $skip\n) {\n  totalItems\n  results {\n    id\n    type\n    registration {\n      status\n      contactNumber\n      trackingId\n      registrationNumber\n      registeredLocationId\n      duplicates\n      assignment {\n        userId\n        firstName\n        lastName\n        officeName\n        __typename\n      }\n      createdAt\n      modifiedAt\n      __typename\n    }\n    operationHistories {\n      operationType\n      operatedOn\n      operatorRole\n      operatorName {\n        firstNames\n        familyName\n        use\n        __typename\n      }\n      operatorOfficeName\n      operatorOfficeAlias\n      notificationFacilityName\n      notificationFacilityAlias\n      rejectReason\n      rejectComment\n      __typename\n    }\n    ... on BirthEventSearchSet {\n      dateOfBirth\n      childName {\n        firstNames\n        familyName\n        use\n        __typename\n      }\n      __typename\n    }\n    ... on DeathEventSearchSet {\n      dateOfDeath\n      deceasedName {\n        firstNames\n        familyName\n        use\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}}",  # noqa: B950
+                        "variables": {
+                            "advancedSearchParameters": {"recordId": recordid}
+                        },
+                    }
+
+                if data:
+                    response = requests.post(
+                        rec.crvs_config_id.domain,
+                        data=json.dumps(data),
+                        headers=headers,
+                    )
+                    if response.status_code == 200:
+                        response_json = response.json()
+                        response_json = response_json["data"]["searchEvents"][
+                            "results"
+                        ][0]
+
+                        _logger.info(response_json)
+                        rec.birthdate = response_json["dateOfBirth"]
+                        rec.given_name = response_json["childName"][0]["firstNames"]
+                        rec.family_name = response_json["childName"][0]["familyName"]
+
+                        message = "Successfully fetched %s, %s with birthdate: %s." % (
+                            rec.family_name,
+                            rec.given_name,
+                            rec.birthdate,
+                        )
+                        kind = "success"
+                        return {
+                            "type": "ir.actions.client",
+                            "tag": "display_notification",
+                            "params": {
+                                "title": _("OpenCRVS"),
+                                "message": message,
+                                "sticky": False,
+                                "type": kind,
+                                "next": {
+                                    "type": "ir.actions.act_window_close",
+                                },
+                            },
+                        }
